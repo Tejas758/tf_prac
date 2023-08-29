9ebeefad2e3680beeacbb4311b3b6997745f6e6e
Tejas758 <tejasjoshi758@gmail.com>
2023-08-29 11:20:00 +0530

    Tejas PC

--- main.tf
+++ main.tf
@@ -9,6 +9,8 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 
+
+
 #======================================
 
 # variable "job-language" {
@@ -37,25 +39,25 @@ resource "aws_s3_bucket" "bucket" {
 
 # for uploading a pyspark file into a bucket
 
-resource "aws_s3_object" "upload-glue-script" {
-  bucket = aws_s3_bucket.bucket.id
-  key    = "< FILE NAME >"
-  source = "< FILE PATH >"
-}
+# resource "aws_s3_object" "upload-glue-script" {
+#   bucket = aws_s3_bucket.bucket.id
+#   key    = "< FILE NAME >"
+#   source = "< FILE PATH >"
+# }
 
-resource "aws_glue_job" "glue_job" {
-  name = "NYC-dataset-processing"
-  role_arn = "< arn >"
-  description = "This is script to convert dataset"
-  max_retries = "1"
-  timeout = 2880
-  command {
-    script_location = "s3://terraform-nikhil-prac/< FILE NAME >"
-    python_version = "3"
-  }
-  execution_property {
-    max_concurrent_runs = 2
-  }
-  glue_version = "4.0"
+# resource "aws_glue_job" "glue_job" {
+#   name = "NYC-dataset-processing"
+#   role_arn = "< arn >"
+#   description = "This is script to convert dataset"
+#   max_retries = "1"
+#   timeout = 2880
+#   command {
+#     script_location = "s3://terraform-nikhil-prac/< FILE NAME >"
+#     python_version = "3"
+#   }
+#   execution_property {
+#     max_concurrent_runs = 2
+#   }
+#   glue_version = "4.0"
   
-}
+# }
